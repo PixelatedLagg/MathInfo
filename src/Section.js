d44a51cd0780aefb07b4331bde9285abcaee8e96
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-07-20 22:03:25 -0400

    + beginning of section parsing

--- src/Section.js
+++ src/Section.js
@@ -1,15 +1,23 @@
 import { Routes, Route, useParams } from 'react-router-dom';
 import React, { Component } from 'react';
-import readJson from './readJson';
 
 export default function Section() {
   var { res } = useParams();
   res = res.toLowerCase();
-  const resources = readJson("data/Resources.json").then((data) => {
-    
+  fetch(`${process.env.PUBLIC_URL}/data/Resources.json`, {headers: {accept: 'application/json'}}).then((response) => response.json()).then((json) => {
+    if (!json.Resources.includes(res))
+    {
+      window.location.href = "https://mathinfo.org";
+    }
+  });
+  fetch(`${process.env.PUBLIC_URL}/data/sections/${res}.json`, {headers: {accept: 'application/json'}}).then((response) => response.json()).then((json) => {
+    const summary = json.Summary;
+    console.log(json);
     return (
-      <div>
-        <p>{res}</p>
+      <div className="content">
+        <div className="text">
+          {summary.text.map((element, content, index) => React.createElement(element, { key: index }, content))}
+        </div>
       </div>
     );
   });
--- src/readJson.js
+++
@@ -1,6 +0,0 @@
-export default async function readJson(source) {
-    return await fetch(`${process.env.PUBLIC_URL}${source}`)
-      .then((response) => {
-        return response.json();
-      });
-}
