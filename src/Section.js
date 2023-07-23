b380fa4e32436cb51b467a01a18d05c97d495674
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-07-22 20:54:43 -0400

    + finally fixed json issue

--- src/Section.js
+++ src/Section.js
@@ -1,24 +1,36 @@
 import { Routes, Route, useParams } from 'react-router-dom';
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 
 export default function Section() {
   var { res } = useParams();
   res = res.toLowerCase();
-  fetch(`${process.env.PUBLIC_URL}/data/Resources.json`, {headers: {accept: 'application/json'}}).then((response) => response.json()).then((json) => {
-    if (!json.Resources.includes(res))
-    {
-      window.location.href = "https://mathinfo.org";
-    }
-  });
-  fetch(`${process.env.PUBLIC_URL}/data/sections/${res}.json`, {headers: {accept: 'application/json'}}).then((response) => response.json()).then((json) => {
-    const summary = json.Summary;
-    console.log(json);
-    /*return (
-      <div className="content">
-        <div className="text">
-          {summary.text.map((element, content, index) => React.createElement(element, { key: index }, content))}
-        </div>
-      </div>
-    );*/
-  });
+  const [resources, setResources] = useState([]);
+  useEffect(() => {
+    import(`./data/Resources.json`)
+      .then((res) => setResources(res.default.Resources))
+      .catch(_ => null);
+  }, []);
+  if (!resources.includes(res) && resources.length != 0)
+  {
+    window.location.href = "https://mathinfo.org";
+  }
+  const [resource, setResource] = useState([]);
+  useEffect(() => {
+    import(`./data/sections/${res}.json`)
+      .then((res) => setResource(res.default))
+      .catch(_ => null);
+  }, []);
+  console.log(resource);
+
+
 }
+
+
+
+/*return (
+        <div className="content">
+          <div className="text">
+            {summary.text.map((element, content, index) => React.createElement(element, { key: index }, content))}
+          </div>
+        </div>
+      );*/
