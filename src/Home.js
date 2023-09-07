487434545156ed8025520a1bdb6ea34790f1fb71
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-09-06 22:41:34 -0400

    + new pythagorean article

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,10 @@
 import './index.css';
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
 export default function Home() {
     document.title = "MathInfo - Home";
     return (
         <div className="content">
-            {/*<Helmet>
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content={window.location.href} />
-                <meta property="og:title" content="Home" />
-                <meta property="og:description" content="MathInfo.org is a cool place to learn about Math and Computer Science." />
-                <meta property="og:image" content="https://mathinfo.org/mathinfologo.png" />
-    </Helmet>*/}
             <div className="text">
                 <h1>Welcome to MathInfo.org</h1>
                 <p>Here you will find (mainly article-based) resources for Mathematics and Computer Science. Each subject is divided into sections, with each section having several articles.</p>
