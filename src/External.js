dcab771da03bed5e22c7f7b6f2af28ff2ef25553
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-09-10 22:26:36 -0400

    + books in external stuff

--- src/External.js
+++ src/External.js
@@ -25,6 +25,18 @@ export default function External() {
                         <a href='https://sophisticatedprimate.com/'>SophisticatedPrimate</a>
                     </li>
                 </ul>
+                <p>Cool books:</p>
+                <ul>
+                    <li>
+                        <a href='https://archive.org/details/ElementsOfAlgebraLeonhardEuler2015/mode/2up'>Elements of Algebra by Leonhard Euler</a>
+                    </li>
+                    <li>
+                        <a href='https://www.amazon.com/Introduction-Number-Theory-Problem-Solving/dp/1934124125'>Introduction to Number Theory (Art of Problem Solving Introduction) by Mathew Crawford</a>
+                    </li>
+                    <li>
+                        <a href='https://archive.org/details/precalculuswithl0000lars_e0m3'>Precalculus with Limits: a Graphing Approach 4th edition by Ron Larson</a>
+                    </li>
+                </ul>
             </div>
         </div>
             );
