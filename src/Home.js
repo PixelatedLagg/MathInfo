af95b0d937d1df242cd64362bd37bdea83d4dbc3
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-02-23 23:56:39 -0500

    general angle trig 2

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ export default function Home(props) {
                 <h1>Welcome to MathInfo.org</h1>
                 <p>Here you will find (mainly article-based) resources for Mathematics and Computer Science, along with some interactive activities to test your understanding.</p>
                 <p>This website was created by Will and Alex, two highschoolers interested in the relating fields. It's their goal to spread the useful knowledge surrounding these topics.</p>
+                <p>Mild disclaimer: we employ AI to enhance the editing process for the majority of our articles. Through its use, we strive to maintain the original structure, core concepts, and intent of the content while focusing on improving clarity and coherence.</p>
             </div>
             <div className="media">
                 <div className="image">
