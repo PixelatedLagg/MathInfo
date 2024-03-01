36941211d1943237c4c27db6e1ca5a27eea8f6e8
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-02-29 23:17:56 -0500

    generate angle beginning

--- src/interactive/Basic-Trigonometry.js
+++ src/interactive/Basic-Trigonometry.js
@@ -10,7 +10,7 @@ function rad(angle) {
 }
 
 
-function generate(_side, setSide)
+function generateSide(_side, setSide)
 {
     var calc = rand(1, 89), side = rand(1, 25);
     if (!Math.round(Math.random()))
@@ -171,20 +171,183 @@ function generate(_side, setSide)
     }
 }
 
+function generateAngle(_side, setAngle)
+{
+    var calc = rand(1, 89), side = rand(1, 25);
+    if (!Math.round(Math.random()))
+    {
+        switch (rand(0, 2))
+        {
+            case 0: //sin = b / c
+                if (!Math.round(Math.random()))
+                {
+                    setAngle({
+                        angle1: `${calc}°`,
+                        angle2: "",
+                        sidea: "",
+                        sideb: side,
+                        sidec: "x",
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `sin(${calc}°) = ${side} / x<br>x * sin(${calc}°) = ${side}<br>x = ${side} / sin(${calc}°)<br>x ≈ ${(side / Math.sin(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                else
+                {
+                    setAngle({
+                        angle1: `${calc}°`,
+                        angle2: "",
+                        sidea: "",
+                        sideb: "x",
+                        sidec: side,
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `sin(${calc}°) = x / ${side}<br>${side} * sin(${calc}°) = x<br>x ≈ ${(side * Math.sin(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                break;
+            case 1: //cos = a / c
+                if (!Math.round(Math.random()))
+                {
+                    setAngle({
+                        angle1: `${calc}°`,
+                        angle2: "",
+                        sideb: "",
+                        sidea: side,
+                        sidec: "x",
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `cos(${calc}°) = ${side} / x<br>x * cos(${calc}°) = ${side}<br>x = ${side} / cos(${calc}°)<br>x ≈ ${(side / Math.cos(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                else
+                {
+                    setAngle({
+                        angle1: `${calc}°`,
+                        angle2: "",
+                        sideb: "",
+                        sidea: "x",
+                        sidec: side,
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `cos(${calc}°) = x / ${side}<br>${side} * cos(${calc}°) = x<br>x ≈ ${(side * Math.cos(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                break;
+            case 2: //tan = b / a
+                if (!Math.round(Math.random()))
+                {
+                    setAngle({
+                        angle1: `${calc}°`,
+                        angle2: "",
+                        sidec: "",
+                        sideb: side,
+                        sidea: "x",
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `tan(${calc}°) = ${side} / x<br>x * tan(${calc}°) = ${side}<br>x = ${side} / tan(${calc}°)<br>x ≈ ${(side / Math.tan(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                else
+                {
+                    setAngle({
+                        angle1: `${calc}°`,
+                        angle2: "",
+                        sidec: "",
+                        sideb: "x",
+                        sidea: side,
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `tan(${calc}°) = x / ${side}<br>${side} * tan(${calc}°) = x<br>x ≈ ${(side * Math.tan(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                break;
+        }
+    }
+    else
+    {
+        switch (rand(0, 2))
+        {
+            case 0: //sin = a / c
+                if (!Math.round(Math.random()))
+                {
+                    setAngle({
+                        angle2: `${calc}°`,
+                        angle1: "",
+                        sideb: "",
+                        sidea: side,
+                        sidec: "x",
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `sin(${calc}°) = ${side} / x<br>x * sin(${calc}°) = ${side}<br>x = ${side} / sin(${calc}°)<br>x ≈ ${(side / Math.sin(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                else
+                {
+                    setAngle({
+                        angle2: `${calc}°`,
+                        angle1: "",
+                        sideb: "",
+                        sidea: "x",
+                        sidec: side,
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `sin(${calc}°) = x / ${side}<br>${side} * sin(${calc}°) = x<br>x ≈ ${(side * Math.sin(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                break;
+            case 1: //cos = b / c
+                if (!Math.round(Math.random()))
+                {
+                    setAngle({
+                        angle2: `${calc}°`,
+                        angle1: "",
+                        sidea: "",
+                        sideb: side,
+                        sidec: "x",
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `cos(${calc}°) = ${side} / x<br>x * cos(${calc}°) = ${side}<br>x = ${side} / cos(${calc}°)<br>x ≈ ${(side / Math.cos(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                else
+                {
+                    setAngle({
+                        angle2: `${calc}°`,
+                        angle1: "",
+                        sidea: "",
+                        sideb: "x",
+                        sidec: side,
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `cos(${calc}°) = x / ${side}<br>${side} * cos(${calc}°) = x<br>x ≈ ${(side * Math.cos(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                break;
+            case 2: //tan = a / b
+                if (!Math.round(Math.random()))
+                {
+                    setAngle({
+                        angle2: `${calc}°`,
+                        angle1: "",
+                        sidec: "",
+                        sidea: side,
+                        sideb: "x",
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `tan(${calc}°) = ${side} / x<br>x * tan(${calc}°) = ${side}<br>x = ${side} / tan(${calc}°)<br>x ≈ ${(side / Math.tan(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                else
+                {
+                    setAngle({
+                        angle2: `${calc}°`,
+                        angle1: "",
+                        sidec: "",
+                        sidea: "x",
+                        sideb: side,
+                        answer: <strong id="solution" tabindex="0" dangerouslySetInnerHTML={{ __html: `tan(${calc}°) = x / ${side}<br>${side} * tan(${calc}°) = x<br>x ≈ ${(side * Math.tan(rad(calc))).toFixed(1)}`}}></strong>
+                    });
+                }
+                break;
+        }
+    }
+}
+
 export default function Trigonometry(props) {
     const [_side, setSide] = useState(0);
+    const [_angle, setAngle] = useState(0);
     document.title = "Basic Trigonometry";
     props.onSetNames("Interactive Basic*Trigonometry");
     props.onSetLinks("/interactive /interactive/basic-trigonometry");
     useEffect(() => {
-        generate(_side, setSide);
+        generateSide(_side, setSide);
+        generateAngle(_angle, setAngle);
     }, []);
     return (
         <div className='content'>
             <div className='section'>
                 <div className="text">
                     <h1>Solve for Side</h1>
-                    <button type="button" className='interactive-button' tabindex="0" onClick={() => generate(_side, setSide)}>Generate New Problem</button>
+                    <button type="button" className='interactive-button' tabindex="0" onClick={() => generateSide(_side, setSide)}>Generate New Problem</button>
                     <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" preserveAspectRatio="xMidYMid meet">
                         <text id="sidea" xmlSpace="preserve" textAnchor="middle" fontSize="24" fontFamily="serif" strokeWidth="0" stroke="#000" fill="#000" y="255" x="40">{_side.sidea}</text>
                         <text id="sideb" xmlSpace="preserve" textAnchor="middle" fontSize="24" fontFamily="serif" strokeWidth="0" stroke="#000" fill="#000" y="465" x="228">{_side.sideb}</text>
@@ -205,7 +368,7 @@ export default function Trigonometry(props) {
             </div>
             <div className='section'>
                 <h1>Solve for Angle</h1>
-                <button type="button" className='interactive-button' tabindex="0" onClick={() => generate(_side, setSide)}>Generate New Problem</button>
+                <button type="button" className='interactive-button' tabindex="0" onClick={() => generateAngle(_angle, setAngle)}>Generate New Problem</button>
                 <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" preserveAspectRatio="xMidYMid meet">
                     <text id="sidea" xmlSpace="preserve" textAnchor="middle" fontSize="24" fontFamily="serif" strokeWidth="0" stroke="#000" fill="#000" y="255" x="40">{_side.sidea}</text>
                     <text id="sideb" xmlSpace="preserve" textAnchor="middle" fontSize="24" fontFamily="serif" strokeWidth="0" stroke="#000" fill="#000" y="465" x="228">{_side.sideb}</text>
