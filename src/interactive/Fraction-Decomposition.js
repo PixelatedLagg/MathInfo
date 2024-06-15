5bcdbfb626081ecb17603716ef14a355fc3ff625
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-06-14 23:59:08 -0400

    vars

--- src/interactive/Fraction-Decomposition.js
+++ src/interactive/Fraction-Decomposition.js
@@ -5,23 +5,41 @@ function randomNum(lower, upper)
     return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
 
+function generateProblem()
+{
+
+}
+
 export default function FractionDecomposition(props) {
+    const [_settings, setSettings] = useState([5, 2, 5, 2]);
+    const [_factor, setFactor] = useState([5, 2, 5, 2]);
     document.title = "Fraction Decomposition";
     props.onSetNames("Interactive Fraction*Decomposition");
     props.onSetLinks("/interactive /interactive/fraction-decomposition");
+    function handleChange(num, index) {
+        if (num > 0)
+        {
+            _settings[index] = num;
+            setSettings(_settings);
+        }
+    }
+    function handleBoolChange(num) {
+        _factor = !_factor;
+        setFactor(_factor);
+    }
     return (
         <div className='content'>
             <div className='section'>
                 <div className="text">
                     <h1>Fraction Decomposition</h1>
                     <div className="checkboxes">
-                        <div className="input-label"><input type="number" id="0" defaultValue={5}/><label htmlFor="0">Maximum Numerator Degrees</label></div>
-                        <div className="input-label"><input type="number" id="1" defaultValue={5}/><label htmlFor="1">Minimum Numerator Degrees</label></div>
-                        <div className="input-label"><input type="number" id="2" defaultValue={5}/><label htmlFor="2">Maximum Denominator Degrees</label></div>
-                        <div className="input-label"><input type="number" id="3" defaultValue={5}/><label htmlFor="3">Minimum Denominator Degrees</label></div>
-                        <div className="input-label"><input type="checkbox" id="4" defaultValue={true}/><label htmlFor="4">Automatically Factor</label></div>
+                        <div className="input-label"><input type="number" id="0" defaultValue={5} onChange={() => handleChange(val, 0)}/><label htmlFor="0">Maximum Numerator Degrees</label></div>
+                        <div className="input-label"><input type="number" id="1" defaultValue={2} onChange={() => handleChange(val, 1)}/><label htmlFor="1">Minimum Numerator Degrees</label></div>
+                        <div className="input-label"><input type="number" id="2" defaultValue={5} onChange={() => handleChange(val, 2)}/><label htmlFor="2">Maximum Denominator Degrees</label></div>
+                        <div className="input-label"><input type="number" id="3" defaultValue={2} onChange={() => handleChange(val, 3)}/><label htmlFor="3">Minimum Denominator Degrees</label></div>
+                        <div className="input-label"><input type="checkbox" id="4" defaultValue={true} onChange={() => handleBoolChange()}/><label htmlFor="4">Automatically Factor</label></div>
                     </div>
-                    <button type="button" className='interactive-button' tabIndex="0">Generate New Problem</button>
+                    <button type="button" className='interactive-button' tabIndex="0" onClick={() => generateProblem()}>Generate New Problem</button>
                 </div>
                 <div className='media'>
                 </div>
