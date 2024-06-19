cca1d70ec69d1981ab12218b1055658de068e8a3
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-06-18 23:01:04 -0400

    division stuffs

--- src/interactive/Fraction-Decomposition.js
+++ src/interactive/Fraction-Decomposition.js
@@ -1,11 +1,25 @@
 import '../index.css';
+import React, { useState, useEffect } from 'react';
+
+let numerator = [];
+let denomerator = [];
 
 function randomNum(lower, upper)
 {
     return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
 
-function generateProblem()
+function divisionPolynomial() //return polynomial result of division
+{
+
+}
+
+function divisionNumerator() //return coefficients of numerator of remainder
+{
+
+}
+
+function divisionDenominator() //return coefficients of denominator of remainder
 {
 
 }
@@ -33,11 +47,7 @@ export default function FractionDecomposition(props) {
                 <div className="text">
                     <h1>Fraction Decomposition</h1>
                     <div className="checkboxes">
-                        <div className="input-label"><input type="number" id="0" defaultValue={5} onChange={() => handleChange(val, 0)}/><label htmlFor="0">Maximum Numerator Degrees</label></div>
-                        <div className="input-label"><input type="number" id="1" defaultValue={2} onChange={() => handleChange(val, 1)}/><label htmlFor="1">Minimum Numerator Degrees</label></div>
-                        <div className="input-label"><input type="number" id="2" defaultValue={5} onChange={() => handleChange(val, 2)}/><label htmlFor="2">Maximum Denominator Degrees</label></div>
-                        <div className="input-label"><input type="number" id="3" defaultValue={2} onChange={() => handleChange(val, 3)}/><label htmlFor="3">Minimum Denominator Degrees</label></div>
-                        <div className="input-label"><input type="checkbox" id="4" defaultValue={true} onChange={() => handleBoolChange()}/><label htmlFor="4">Automatically Factor</label></div>
+                        
                     </div>
                     <button type="button" className='interactive-button' tabIndex="0" onClick={() => generateProblem()}>Generate New Problem</button>
                 </div>
@@ -47,3 +57,11 @@ export default function FractionDecomposition(props) {
         </div>
         );
 }
+
+/*
+<div className="input-label"><input type="number" id="0" defaultValue={5} onChange={() => handleChange(val, 0)}/><label htmlFor="0">Maximum Numerator Degrees</label></div>
+                        <div className="input-label"><input type="number" id="1" defaultValue={2} onChange={() => handleChange(val, 1)}/><label htmlFor="1">Minimum Numerator Degrees</label></div>
+                        <div className="input-label"><input type="number" id="2" defaultValue={5} onChange={() => handleChange(val, 2)}/><label htmlFor="2">Maximum Denominator Degrees</label></div>
+                        <div className="input-label"><input type="number" id="3" defaultValue={2} onChange={() => handleChange(val, 3)}/><label htmlFor="3">Minimum Denominator Degrees</label></div>
+                        <div className="input-label"><input type="checkbox" id="4" defaultValue={true} onChange={() => handleBoolChange()}/><label htmlFor="4">Automatically Factor</label></div>
+*/
