cb33536e24cceabd0290a2f1fc03c6f95f635161
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-10-14 23:01:28 -0400

    some fixes

--- src/interactive.js
+++ src/interactive.js
@@ -20,15 +20,14 @@ export default function Interactive(props)
             </Helmet>
             <div className="home-alltext" style={{marginBottom: "32px"}}>
                 <h1>Interactive</h1>
-                <p>Interactive activities allow you to test your math skills with randomly generated problems.</p>
+                <p>Interactive activities allow you to test your math skills with randomly generated problems. Activities vary from algebra to trigonometry to calculus. Email admin@mathinfo.org for requests.</p>
             </div>
             <div className="divider"></div>
             <div className="article-grid">
                 <Link to='/interactive/basic-trigonometry' aria-label="Visit Basic Trigonometry Page">Basic Trigonometry</Link>
                 <Link to='/interactive/statistics-categories' aria-label="Visit Statistics Categories Page">Statistics Categories</Link>
+                <Link to='/interactive/polynomial-division' aria-label="Visit Polynomial Division Page">Polynomial Division</Link>
             </div>
         </div>
     );
 }
-
-//<Link to='/interactive/polynomial-division' aria-label="Visit Polynomial Division Page">Polynomial Division</Link>
